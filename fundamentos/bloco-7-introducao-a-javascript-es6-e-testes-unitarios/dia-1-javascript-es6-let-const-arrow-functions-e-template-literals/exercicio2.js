c35a1acec8a66e364516eb084b101af78568ce44
Jvieyrah <keep@thinkeep-5260>
2022-03-04 13:41:39 -0300

    exercicios da mentoria

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio2.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio2.js
@@ -10,20 +10,35 @@
 
 // console.log(factorial(4));
 
-const longestWord = text  => {
-    let wordArray = text.split(" ");
-    let maxLength = 0;
-    let result = "";
+// const longestWord = text  => {
+//     let wordArray = text.split(" ");
+//     let maxLength = 0;
+//     let result = "";
     
-    for (const word of wordArray){
-if (word.length > maxLength) {
-maxLength= word.length;
-result=word
-console.log (result);
-}
-    }
-    return result;
-}
+//     for (const word of wordArray){
+// if (word.length > maxLength) {
+// maxLength= word.length;
+// result=word
+// console.log (result);
+// }
+//     }
+//     return result;
+// }
   
 
-console.log(longestWord("Antonio foi no banheiro e não sabemos o que aconteceu com ele"));
+// console.log(longestWord("Antonio foi no banheiro e não sabemos o que aconteceu com ele"));
+
+// const isEven = n => n%2 ===0; 
+// console.log(isEven(2+4));
+
+const somaArrayPares = array => {
+let result= 0;
+for (let numero of array){
+    result+= numero %2 === 0? numero: 0; 
+        console.log(result);
+   }
+
+return result;
+} 
+
+console.log(somaArrayPares([3,3,3]));
