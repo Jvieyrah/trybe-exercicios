95fd7d719397fd88136513cfa9f7fb396ff69b88
Joao <me.contrate.aero@gmail.com>
2022-10-04 23:24:14 -0300

    exercicio 3

--- back-end/back-end/TypeScript/dia_02:_tipagem_estática_e_Generics/index.ts
+++ back-end/back-end/TypeScript/dia_02:_tipagem_estática_e_Generics/index.ts
@@ -1,7 +1,7 @@
 import Car from "./Car";
+import Pizza from "./Pizza";
 
 const gol = new Car("Volkswagen", "prata", 4);
-
 
 gol.openTheDoor(Doors.DRIVER);
 gol.closeTheDoor(Doors.DRIVER);
@@ -29,3 +29,26 @@ gol.turn("direita");
 gol.speedDown();
 gol.stop();
 gol.speedUp();
+
+const calabresa: Pizza = {
+    flavor: "Calabresa",
+    slices: 8
+  }
+
+  console.log(calabresa);
+
+  const marguerita: Pizza = {
+    flavor: "Marguerita",
+    slices: 6
+  }
+  
+  console.log(marguerita);
+  
+  
+  const nutela: Pizza = {
+    flavor: "Nutela",
+    slices: 4
+  }
+  
+  console.log(nutela);
+  
