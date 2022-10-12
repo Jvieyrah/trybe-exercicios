ac2cd9779e6a554b33c2ac250e32c1d8f6b85add
Joao <me.contrate.aero@gmail.com>
2022-10-12 15:23:30 -0300

    exercicios

--- back-end/back-end/POO/dia1-introdu-oo/fastfood.ts
+++ back-end/back-end/POO/dia1-introdu-oo/fastfood.ts
@@ -92,3 +92,88 @@ class Item {
         this._price = price;
     }
 }
+
+class Data {
+    private _day: number;
+    private _month: number;
+    private _year: number;
+
+    constructor(day: number, month: number, year: number) {
+       const dateStr = `${day}/${month}/${year}`;
+
+       if(new Date(dateStr).getDate() !== day){
+        this._day = 1;
+        this._month = 1;
+        this._year = 1900;
+       } else {
+        this._day = day;
+        this._month = month;
+        this._year = year;
+       }
+
+    }
+
+    get day(): number {
+        return this._day;
+    }
+
+    set day(day: number) {
+        this._day = day;
+    }
+
+    get month(): number {
+        return this._month;
+    }
+
+    set month(month: number) {
+        this._month = month;
+    }
+
+    getMonthName(): string {
+        const months = [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December',
+        ];
+
+        return months[this._month - 1];
+    }
+
+    get year(): number {
+        return this._year;
+    }
+
+    isLeapYear(): boolean {
+        if(this._year % 4 === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    compareDate(date: Data): number {
+        const date1 = new Date(`${this._day}/${this._month}/${this._year}`);
+        const date2 = new Date(`${date.day}/${date.month}/${date.year}`);
+
+        if(date1.getTime() > date2.getTime()) {
+            return 1;
+        } else if(date1.getTime() < date2.getTime()) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+} 
+
+const testDate = new Data(29, 1, 1989);
+
+console.log(testDate);
