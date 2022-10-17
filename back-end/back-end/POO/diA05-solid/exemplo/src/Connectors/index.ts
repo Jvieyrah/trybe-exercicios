570ac40c6102d3c0ee1127391ecd1b6d845e58e1
Joao <me.contrate.aero@gmail.com>
2022-10-17 20:27:26 -0300

    conclusão do exercicio

--- back-end/back-end/POO/diA05-solid/exemplo/src/Connectors/index.ts
+++ back-end/back-end/POO/diA05-solid/exemplo/src/Connectors/index.ts
@@ -1,11 +1,14 @@
-import Connector, { ConnectorConstructor } from "./Connector";
+import Connector, { ReadOnlyConnector,ConnectorConstructor } from "./Connector";
 import MySQLConnector from "./mysql/MySQLConnector";
 import RedisConnector from "./redis/RedisConnector";
+import ReadOnlyRedisConnector from "./redis/ReadOnlyRedisConnector";
 
 export default Connector;
 export {
   Connector,
   ConnectorConstructor,
   MySQLConnector,
-  RedisConnector
+  RedisConnector,
+  ReadOnlyRedisConnector,
+  ReadOnlyConnector
 };
