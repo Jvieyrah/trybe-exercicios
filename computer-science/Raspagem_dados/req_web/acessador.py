3c9ae1ff3a5b25ffc13d78e095695774f8d5b1ad
Joao <me.contrate.aero@gmail.com>
2023-02-26 22:04:15 -0300

    update

--- computer-science/Raspagem_dados/req_web/acessador.py
+++ computer-science/Raspagem_dados/req_web/acessador.py
@@ -7,5 +7,6 @@ thumb_url_selector = "div.image_container a::attr(href)"
 for url in selector.css(thumb_url_selector).getall():
     thumb_url_request = requests.get("http://books.toscrape.com/" + url)
     thumb_url_selector = Selector(text=thumb_url_request.text)
-    book_title = thumb_url_selector.css("div.product_main h1::text")
-    print(book_title.get())
+    book_title = thumb_url_selector.css("div.product_main h1::text").get()
+    price = thumb_url_selector.css(".price_color::text").get()
+    print(book_title, price)
